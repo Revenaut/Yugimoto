121684a7f021bf82afa9bfd79e2f06739b5c5aa1
Revenarus <bcjacobs1@nps.edu>
2017-08-26 11:30:08 -0400

    working model

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ class Manager(object):
         self.board = [None]*14
     def addCard(self, elem, card):
         self.board[elem] = card
+
+
+def main():
+    manager = Manager()
+    manager.addCard(1, "test")
+    print(manager.board[1])
+
+if __name__ == "__main__":
+    main()
