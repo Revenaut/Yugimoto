e81024a7589968d95df06e7b56df216e1ca7f627
Revenarus <bcjacobs1@nps.edu>
2017-08-26 13:21:19 -0400

    i'm a genious

--- deck.py
+++ deck.py
@@ -1,12 +1,17 @@
 from random import shuffle
+
+
 class Deck(object):
     def __init__(self):
         self.contents = self.genContents()
-    def genContents(self):
+
+    def genContents(self):  # generate a list of [0..13] 4 times.
         contents = []
         for x in range(13):
             for i in range(4):
                 contents.append(x)
         return contents
+
+    # randomly arrange the order of the elements in the deck list.
     def shuffleDeck(self):
         shuffle(self.contents)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from manager import *
 from deck import *
+from random import randint
+
 
 def main():
     manager = Manager()
-    print(manager.playerA)
-    print(manager.playerB)
-    print(len(manager.deck.contents))
+
 
 if __name__ == "__main__":
     main()
