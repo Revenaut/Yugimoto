c2cc6da28cd2d0737206c257eef62a1d83067295
Revenarus <bcjacobs1@nps.edu>
2017-08-26 11:20:52 -0400

    Rudimentary game state manager.

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-print("param value, param ..., param sep, param end, param file, param flush")
-
+class Manager(object):
+    def __init__(self):
+        self.board = [None]*14
+    def addCard(self, elem, card):
+        self.board[elem] = card
