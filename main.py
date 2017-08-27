799e94fc86d7b8964779bf011cd8671842dd13e6
Le Terally <leterally@gmail.com>
2017-08-26 22:37:48 -0700

    Changed main function.

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ from random import randint
 
 
 def main():
-    hand = Hand()
-    deck = Deck()
-    hand.add(deck.contents[0])
-    hand.add(deck.contents[1])
-    print(hand.show)
-    print(hand.value)
+    Logger.setFile('log.txts')
 
 if __name__ == "__main__":
     main()
