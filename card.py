e344d6239c86e6ba3a3b13a5e6ae442d8331bec7
Le Terally <leterally@gmail.com>
2017-08-26 22:15:09 -0700

    And promptly moved that shit back.

--- card.py
+++ card.py
@@ -1,3 +1,26 @@
+RANKS = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11
+}
+
+SUITS = {
+    "Clubs": 0,
+    "Diamonds": 1,
+    "Hearts": 2,
+    "Spades": 3
+}
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
--- hand.py
+++ hand.py
@@ -1,28 +1,5 @@
 from card import *
 
-RANKS = {
-    "Two": 2,
-    "Three": 3,
-    "Four": 4,
-    "Five": 5,
-    "Six": 6,
-    "Seven": 7,
-    "Eight": 8,
-    "Nine": 9,
-    "Ten": 10,
-    "Jack": 10,
-    "Queen": 10,
-    "King": 10,
-    "Ace": 11
-}
-
-SUITS = {
-    "Clubs": 0,
-    "Diamonds": 1,
-    "Hearts": 2,
-    "Spades": 3
-}
-
 class Hand:
     def __init__(self):
         self.contents = [] # list of cards
