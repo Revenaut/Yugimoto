b584f928dcb1ec823d4763a303802a461c3dcae8
Le Terally <leterally@gmail.com>
2017-08-26 23:53:44 -0700

    logger comments

--- logger.py
+++ logger.py
@@ -3,10 +3,12 @@ import os.path
 LOGS_PATH = 'logs'
 
 class Logger:
+    # makes the logger output to the given filename
     @staticmethod
     def setFile(filename):
         Logger.file = open(os.path.join(LOGS_PATH, filename), 'w')
 
+    # writes a message to the file
     @staticmethod
     def log(message):
         Logger.file.write(message + "\n")
