299e4bfcb025bf1db5ac3f43560c3c86dbe0d3d5
Le Terally <leterally@gmail.com>
2017-08-26 23:51:50 -0700

    Example main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from manager import *
-from deck import *
-from hand import *
-from random import randint
+from game import *
+from logger import *
 
 
 def main():
     Logger.setFile('log.txt')
+    game = Game()
+    game.addPlayer('Jack')
+    game.addPlayer('Ben')
+    game.deal()
 
 if __name__ == "__main__":
     main()
