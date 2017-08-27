987b899132a6e1b63d1f62df13578343c7e174ed
Le Terally <leterally@gmail.com>
2017-08-26 22:14:15 -0700

    Moved rank and suit values to hand class.

--- card.py
+++ card.py
@@ -1,34 +1,7 @@
-RANKS = {
-    "Two": 2,
-    "Three": 3,
-    "Four": 4,
-    "Five": 5,
-    "Six": 6,
-    "Seven": 7,
-    "Eight": 8,
-    "Nine": 9,
-    "Ten": 10,
-    "Jack": 10,
-    "Queen": 10,
-    "King": 10,
-    "Ace": 11
-}
-
-SUITS = {
-    "Clubs": 0,
-    "Diamonds": 1,
-    "Hearts": 2,
-    "Spades": 3
-}
-
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-
-    @property
-    def value(self):
-        return RANKS[self.rank]
 
     @property
     def show(self):
--- hand.py
+++ hand.py
@@ -1,5 +1,28 @@
 from card import *
 
+RANKS = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11
+}
+
+SUITS = {
+    "Clubs": 0,
+    "Diamonds": 1,
+    "Hearts": 2,
+    "Spades": 3
+}
+
 class Hand:
     def __init__(self):
         self.contents = [] # list of cards
@@ -11,8 +34,8 @@ class Hand:
         self.contents.append(card)
         if card.rank == "Ace":
             self.softness += 1
-        self.value += card.value
-        if self.value > 21 and softness > 0:
+        self.value += RANKS[card.rank]
+        if self.value > 21 and self.softness > 0:
             self.value -= 10
             self.softness -= 1
 
