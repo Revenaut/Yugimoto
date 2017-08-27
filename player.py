710086dd83d9b793d8937ee22d7fb4fea3306efd
Le Terally <leterally@gmail.com>
2017-08-26 22:51:57 -0700

    Added hit function

--- player.py
+++ player.py
@@ -1,7 +1,13 @@
 from deck import *
 from hand import *
+from logger import *
 
 class Player:
     def __init__(self, name):
         self.hand = Hand()
         self.name = name
+
+    def hit(self, deck):
+        card = deck.draw()
+        Logger.log(self.name + " drew " + card.fullName + ".")
+        self.hand.add(card)
